cea37389581694e74729ba93fe6fe0140281b44a
Anusha Zubair <anushazubair17@gmail.com>
2024-03-30 16:20:52 +0500

    up

--- app/app.py
+++ app/app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 # Load the trained model
 model = joblib.load("model.pkl")  # Ensure this matches your saved model file name
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     prediction = model.predict([data['features']])
-    return jsonify({'prediction': prediction.tolist()})
+    response = {'prediction': prediction.tolist()}
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
